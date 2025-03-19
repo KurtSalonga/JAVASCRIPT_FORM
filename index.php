dcd5fd2891806359b1ab16fc903a655910b0314c
KurtSalonga <kasalonga23-0391@cca.edu.ph>
2025-03-19 12:04:18 +0800

    INDEX.PHP
    UPDATED INDEX.PHP FILE

--- index.php
+++ index.php
@@ -3,15 +3,15 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Javascript Form</title>
+	<title>JAVASCRIPT FORM</title>
 </head>
 <body>
 <form action="action_page.php" method="POST">
   <fieldset>
-    <legend>Personalia:</legend>
-    <label for="fname">First name:</label>
+    <legend>Information:</legend>
+    <label for="fname">First Name:</label>
     <input type="text" id="fname" name="fname"><br><br>
-    <label for="lname">Last name:</label>
+    <label for="lname">Last Name:</label>
     <input type="text" id="lname" name="lname"><br><br>
     <label for="email">Email:</label>
     <input type="email" id="email" name="email"><br><br>
